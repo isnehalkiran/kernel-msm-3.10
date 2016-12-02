b812246547ecfb667afae5fb589e9427e70ea64b
TheStrix <parthbhatia98@gmail.com>
2016-12-03 00:12:10 +0530

    ASoC: compress: Don't error on both playback and capture being set
    * Partial revert of following commit from upstream:
      https://git.kernel.org/cgit/linux/kernel/git/stable/linux-stable.git/commit/?h=linux-3.10.y&id=9a76e683b64361450f3e331dd6634f5aa39ea51b
    
      QCOM sets both capabilities on CPU side as well, and we don't want to error on this.
    
    Change-Id: Ie9ee6298312e43d3e1dd7d73b0b757d46c0da732
    Signed-off-by: TheStrix <parthbhatia98@gmail.com>

--- sound/soc/soc-compress.c
+++ sound/soc/soc-compress.c
@@ -801,6 +801,14 @@ int soc_new_compress(struct snd_soc_pcm_runtime *rtd, int num)
 			rtd->dai_link->stream_name, codec_dai->name, num);
 
 	if (codec_dai->driver->playback.channels_min)
+		playback = 1;
+	if (codec_dai->driver->capture.channels_min)
+		capture = 1;
+
+	capture = capture && cpu_dai->driver->capture.channels_min;
+	playback = playback && cpu_dai->driver->playback.channels_min;
+
+	if(playback)
 		direction = SND_COMPRESS_PLAYBACK;
 	else if (codec_dai->driver->capture.channels_min)
 		direction = SND_COMPRESS_CAPTURE;
